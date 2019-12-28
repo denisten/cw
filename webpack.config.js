dadb4412afecee72304d3ea0e2512f2cbaa4a77d
Тен Денис Сергеевич <dten@stream.ru>
2019-12-28 03:50:03 +0300

    removed comments from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,6 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/
             },
-            // {
-            //     test: /\.js$/,
-            //     exclude: /node_modules/,
-            //     loader: 'babel-loader'
-            // },
             {
                 test: /\.(png|jpeg|jpg|icon|gif)$/,
                 use: [{
