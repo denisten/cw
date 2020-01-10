6a3a3c4f631cc90e78137b6fad6de2fcf9298ccd
Тен Денис Сергеевич <dten@stream.ru>
2020-01-10 16:01:09 +0300

    added precommit and prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import ScrollContainer from 'react-indiana-drag-scroll'
-import styled from "styled-components";
+import styled from 'styled-components'
 import map from './img/map/map.png'
-import {FirstTower} from "./img/buildings/first-building";
-import {ModalWindow} from "./components/modal-window";
-import {useStore} from "effector-react";
-import {AppConditionState} from "./effector/app-condition/store";
+import { FirstTower } from './img/buildings/first-building'
+import { ModalWindow } from './components/modal-window'
+import { useStore } from 'effector-react'
+import { AppConditionState } from './effector/app-condition/store'
 
 const ComponentWrapper = styled.div`
-  border: solid 5px #E2D7C7;
+  border: solid 5px #e2d7c7;
   background: #282828;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-`;
+`
 
 const ScrollContainerStyle = {
-  height: "100%",
-  width: "100%"
-};
+  height: '100%',
+  width: '100%',
+}
 
 const MapWrapper = styled.div`
   background-image: url(${map});
@@ -29,28 +29,31 @@ const MapWrapper = styled.div`
   position: relative;
   top: 0;
   left: 0;
-`;
-
-
+`
 
 export const App = () => {
+  const { isModalWindowOpen } = useStore(AppConditionState)
 
-    const {isModalWindowOpen} = useStore(AppConditionState);
-
-        return (
-            <ComponentWrapper>
-                {isModalWindowOpen ? <ModalWindow/> : ''}
-                <ScrollContainer
-                    style={ScrollContainerStyle}
-                    nativeMobileScroll={false}
-                    onStartScroll={(...args) => { console.log('onStartScroll', args) }}
-                    onScroll={(...args) => { console.log('onScroll', args) }}
-                    onEndScroll={(...args) => { console.log('onEndScroll', args) }}
-                >
-                    <MapWrapper>
-                        <FirstTower/>
-                    </MapWrapper>
-                </ScrollContainer>
-            </ComponentWrapper>
-        )
+  return (
+    <ComponentWrapper>
+      {isModalWindowOpen ? <ModalWindow /> : ''}
+      <ScrollContainer
+        style={ScrollContainerStyle}
+        nativeMobileScroll={false}
+        onStartScroll={(...args) => {
+          console.log('onStartScroll', args)
+        }}
+        onScroll={(...args) => {
+          console.log('onScroll', args)
+        }}
+        onEndScroll={(...args) => {
+          console.log('onEndScroll', args)
+        }}
+      >
+        <MapWrapper>
+          <FirstTower />
+        </MapWrapper>
+      </ScrollContainer>
+    </ComponentWrapper>
+  )
 }
