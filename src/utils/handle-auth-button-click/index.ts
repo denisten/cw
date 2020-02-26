079e6f576022dae49c79a4e8ab305f00c0c1a8c8
Тен Денис Сергеевич <dten@stream.ru>
2020-02-26 14:15:24 +0300

    selfreview

--- src/utils/handle-auth-button-click/index.ts
+++ src/utils/handle-auth-button-click/index.ts
@@ -1,8 +1,8 @@
-import { lcUpdateHandler } from '../lc-update-handler';
 import { getUrl } from '../../api';
+import { editIsAuthorizedFlag } from '../../effector/app-condition/events';
 
 export const handleAuthButtonClick = async () => {
-  window.addEventListener('storage', lcUpdateHandler);
+  window.addEventListener('storage', () => editIsAuthorizedFlag(true));
   const authWindow = window.open(
     undefined,
     'window_auth',
