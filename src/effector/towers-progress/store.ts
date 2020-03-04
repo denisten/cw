55ff68aa6027a127b7752f1c7ff70373bd719308
Тен Денис Сергеевич <dten@stream.ru>
2020-03-04 15:26:28 +0300

    returned to low level condition library tower

--- src/effector/towers-progress/store.ts
+++ src/effector/towers-progress/store.ts
@@ -52,7 +52,7 @@ const initState: TowersProgressStoreType = {
     progress: 0,
   },
   [TowersTypes.LIBRARY]: {
-    level: TowerLevel.high,
+    level: TowerLevel.low,
     progress: 0,
   },
   [TowersTypes.OBSERVATORY]: {
