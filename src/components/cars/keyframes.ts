93e7dec642bf5f581819b43d0656eae335401d29
Dvasilkov <DVasilkov@stream.ru>
2020-03-04 12:47:30 +0300

    fix cars anim

--- src/components/cars/keyframes.ts
+++ src/components/cars/keyframes.ts
@@ -6,7 +6,7 @@ import { keyframes } from 'styled-components';
 export const animForCar1 = keyframes`
            0% {
             left: 25.5%;
-            top: 45.2%;
+            top: 45.1%;
              background: url(${topRightBg}) no-repeat center;
           }
           49% {
@@ -14,8 +14,8 @@ export const animForCar1 = keyframes`
           display: flex;
           }
           50% {
-            left: 36.1%;
-            top: 38%;
+            left: 35.9%;
+            top: 37.75%;
              background: url(${bottomLeftBg}) no-repeat center;
              display: none;
           }
@@ -35,7 +35,7 @@ export const animForCar1 = keyframes`
 export const animForCar2 = keyframes`
           0% {
             left: 35.4%;
-            top: 39.2%;
+            top: 39.1%;
              background: url(${bottomRightBg}) no-repeat center;
           }
           49% {
@@ -44,7 +44,7 @@ export const animForCar2 = keyframes`
           }
           50% {
             left: 42.4%;
-            top: 44.3%;
+            top: 44.1%;
              background: url(${topLeftBg}) no-repeat center;
           display: none;
           }
@@ -54,7 +54,7 @@ export const animForCar2 = keyframes`
           }
         100% {
           left: 35.4%;
-          top: 39.2%;
+          top: 39.1%;
           background: url(${bottomRightBg}) no-repeat center;
           display: none;
         }
@@ -62,7 +62,7 @@ export const animForCar2 = keyframes`
 export const animForCar3 = keyframes`
           0% {
             left: 58.4%;
-            top: 48.2%;
+            top: 47.85%;
              background: url(${bottomRightBg}) no-repeat center;
             
           }
@@ -84,7 +84,7 @@ export const animForCar3 = keyframes`
         }
         100% {
           left: 58.1%;
-          top: 48.2%;
+          top: 47.85%;
           background: url(${bottomRightBg}) no-repeat center;
           
           display: none;
