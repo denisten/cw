0cee0950bf49500f6ae880d2de2c88188ab57c66
Dvasilkov <DVasilkov@stream.ru>
2020-03-13 11:12:45 +0300

    fix

--- src/components/tower-info-task/index.tsx
+++ src/components/tower-info-task/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { TowerInfoDescription } from '../tower-info-description';
 
 export const TowerInfoTask = () => {
-  return <div>Task</div>;
+  return (
+    <TowerInfoDescription
+      text={`Скоро тут появятся задания, выполняя которые, ты сможешь улучшать свой
+    город и зарабатывать валюту (название)`}
+    ></TowerInfoDescription>
+  );
 };
