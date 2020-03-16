8001978c6e6efe4c550cd1fb097acbbd84383bf5
Тен Денис Сергеевич <dten@stream.ru>
2020-03-16 14:04:28 +0300

    returned tutorial

--- src/effector/tutorial-store/store.ts
+++ src/effector/tutorial-store/store.ts
@@ -38,8 +38,8 @@ const TutorialSteps = [
 ];
 
 const initState = {
-  tutorialCondition: TutorialConditions.OFF,
-  tutorialConditionIdx: 0,
+  tutorialCondition: TutorialConditions.DIALOG_HELLO,
+  tutorialConditionIdx: 1,
   tutorialTextId: 0,
 };
 
