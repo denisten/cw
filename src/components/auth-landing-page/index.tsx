eaeafb622880b5c220e871f721bef0bdba3dfe39
Dvasilkov <DVasilkov@stream.ru>
2020-03-20 10:15:05 +0300

    in progress

--- src/components/auth-landing-page/index.tsx
+++ src/components/auth-landing-page/index.tsx
@@ -2,11 +2,15 @@ import { useEffect } from 'react';
 import { authCookieKey, CookieService } from '../../sevices/cookies';
 import { editIsAuthorizedFlag } from '../../effector/app-condition/events';
 
-const delayBeforeAuthWindowClose = 1000;
+const delayBeforeAuthWindowClose = 100000;
 
 export const AuthLandingPage = () => {
+  // const locationSearch = window.location.search;
+  // const search = new URLSearchParams(locationSearch);
+
   useEffect(() => {
     const cookie = CookieService.idToken;
+
     if (cookie) {
       localStorage.setItem(authCookieKey, cookie);
       editIsAuthorizedFlag(true);
