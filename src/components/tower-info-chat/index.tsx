f8965122ab980c3f4694ea2ce936b0bc1a72b2e0
Тен Денис Сергеевич <dten@stream.ru>
2020-03-24 13:19:52 +0300

    hotfix chat discription

--- src/components/tower-info-chat/index.tsx
+++ src/components/tower-info-chat/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TowerInfoDescription } from '../tower-info-description';
-const chatText = ['Скоро тут появятся чат с виртуальным помощником'];
+const chatText = ['Скоро тут появится чат с виртуальным помощником'];
 
 export const TowerInfoChat = () => {
   return <TowerInfoDescription text={chatText} />;
