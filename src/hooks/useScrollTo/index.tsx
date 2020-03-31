88f0a50bd2dbf14c66ba5c58dd58e830d8003383
Dvasilkov <DVasilkov@stream.ru>
2020-03-31 16:51:01 +0300

    убрал лишние срабатывания useEffect в useScrollTo. При ресайзе карты не скроллится к центральному зданию каждый раз

--- src/hooks/useScrollTo/index.tsx
+++ src/hooks/useScrollTo/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const useScrollTo = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -6,11 +6,29 @@ export const useScrollTo = (
   scrollCoords: number[],
   dependency: (string | number | boolean)[]
 ) => {
+  const arrayRef = useRef<number[] | null>(null);
+  const arrayDiff = (arrayOne: number[], arrayTwo: number[]) => {
+    return arrayOne
+      .filter(i => !arrayTwo.includes(i))
+      .concat(arrayTwo.filter(i => !arrayOne.includes(i)));
+  };
+  const scrollToPosition = () => {
+    node.style.scrollBehavior = 'smooth';
+    node.scrollTo(...scrollCoords);
+    node.style.scrollBehavior = '';
+    arrayRef.current = scrollCoords;
+  };
   useEffect(() => {
-    if (node) {
-      node.style.scrollBehavior = 'smooth';
-      node.scrollTo(...scrollCoords);
-      node.style.scrollBehavior = '';
+    if (arrayRef.current === null && node) {
+      // first start
+      scrollToPosition();
+    } else if (
+      arrayRef.current !== null &&
+      node &&
+      arrayDiff(arrayRef.current, scrollCoords).length !== 0
+    ) {
+      // значения в scrollCoords изменились
+      scrollToPosition();
     }
   }, [...dependency, node, scrollCoords]);
 };
