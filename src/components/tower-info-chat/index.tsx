6c9668381ff4fd9d85a688b995633b765692efc9
DVasilkov <dvasilkov@stream.ru>
2020-04-03 16:05:07 +0300

    added buttons

--- src/components/tower-info-chat/index.tsx
+++ src/components/tower-info-chat/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import whiteCorner from './corner-white.svg';
+import bCorner from './corner-b.svg';
 
 const ChatWrapper = styled.div`
   width: 100%;
@@ -18,6 +20,7 @@ const ChatWrapper = styled.div`
     height: 40px;
     display: block;
     background-image: linear-gradient(to top, rgba(243, 243, 243, 0), #fbfbfb);
+    z-index: 2;
   }
 `;
 
@@ -31,21 +34,43 @@ const MessageRow = styled.div<{ type?: string }>`
 
 const Message = styled.div<{ type?: string }>`
   margin: ${props => (props.type === 'system' ? '0 0 0 8px' : '0 8px 0 0')};
-  background-color: ${props => (props.type === 'system' ? 'white' : '#02adc9')};
-  border-radius: ${props =>
-    props.type === 'system' ? '32px 32px 32px 0px' : '32px 32px 0px 32px'};
+  background-color: ${props => (props.type === 'system' ? 'white' : '#04B5D2')};
+  border-radius: 4px;
   padding: ${props =>
     props.type === 'system' ? '12px 41px 12px 29px' : '22px 41px 22px 24px'};
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   box-shadow: 0 4px 11px 0 #e2e5eb;
+  position: relative;
 
   span {
     font-family: MTSSans;
     font-size: 16px;
     line-height: 1.25;
     color: ${props => (props.type === 'system' ? '#001424' : 'white')};
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    background: url(${whiteCorner}) no-repeat center;
+    bottom: 0;
+    left: -6px;
+    width: 38px;
+    height: 32px;
+    display: ${props => (props.type === 'system' ? 'block' : 'none')};
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    background: url(${bCorner}) no-repeat center;
+    bottom: 0;
+    right: -6px;
+    width: 38px;
+    height: 32px;
+    display: ${props => (props.type === 'user' ? 'block' : 'none')};
   }
 `;
 
@@ -68,7 +93,7 @@ const Avatar = styled.img`
   background: red;
 `;
 
-const chatConfig = {
+const ChatConfig: IChatConfig = {
   systemBotAvatar: '',
   userAvatar: '',
   messages: [
@@ -106,28 +131,73 @@ const chatConfig = {
       type: 'user',
     },
   ],
+  buttons: [
+    { title: 'Ничего себе, расскажи подробнее!', answerId: 12 },
+    { title: 'Ясно понятно', answerId: 14 },
+    { title: 'Пойду поем', answerId: 16 },
+  ],
 };
+
+const Button = styled.div`
+  border-radius: 4px;
+  box-shadow: 0 1px 3px 0 #bbc1c7;
+  background-color: #04b5d2;
+  padding: 4px 16px;
+  margin: 0 12px 12px 0;
+  color: white;
+  cursor: pointer;
+`;
+
+const ButtonBody = styled.div`
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`;
 
 export const TowerInfoChat = () => {
   const setAvatar = (messageType: string) => {
     if (messageType === 'system') {
-      return chatConfig.systemBotAvatar;
+      return ChatConfig.systemBotAvatar;
     } else {
-      return chatConfig.userAvatar;
+      return ChatConfig.userAvatar;
     }
   };
 
+  const sendAnswerId = (id: number) => {
+    // do somethink
+  };
+
   return (
-    <ChatWrapper>
-      {chatConfig.messages.map(item => (
-        <MessageRow key={item.id} type={item.type}>
-          <Avatar src={setAvatar(item.type)}></Avatar>
-          <Message type={item.type}>
-            {item.type === 'system' && <BotName>{item.botName}</BotName>}
-            <span>{item.text}</span>
-          </Message>
-        </MessageRow>
-      ))}
-    </ChatWrapper>
+    <>
+      <ChatWrapper>
+        {ChatConfig.messages.map(item => (
+          <MessageRow key={item.id} type={item.type}>
+            <Avatar src={setAvatar(item.type)}></Avatar>
+            <Message type={item.type}>
+              {item.type === 'system' && <BotName>{item.botName}</BotName>}
+              <span>{item.text}</span>
+            </Message>
+          </MessageRow>
+        ))}
+      </ChatWrapper>
+      <ButtonBody>
+        {ChatConfig.buttons.map(button => (
+          <Button
+            key={button.answerId}
+            onClick={() => sendAnswerId(button.answerId)}
+          >
+            {button.title}
+          </Button>
+        ))}
+      </ButtonBody>
+    </>
   );
 };
+
+interface IChatConfig {
+  systemBotAvatar?: string;
+  userAvatar?: string;
+  messages: { id: number; text: string; type: string; botName?: string }[];
+  buttons: { title: string; answerId: number }[];
+}
