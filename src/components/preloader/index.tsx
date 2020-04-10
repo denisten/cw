8b670b6c07412eddda9ac82c68f7a8dc0340b4ec
DVasilkov <dvasilkov@stream.ru>
2020-04-10 14:13:09 +0300

    add preloader (img)

--- src/components/preloader/index.tsx
+++ src/components/preloader/index.tsx
@@ -3,6 +3,7 @@ import styled, { keyframes } from 'styled-components';
 import { ZIndexes } from '../root-component/z-indexes-enum';
 import { cloudsConfig } from './clouds-config';
 import background from './background.jpg';
+import { MTSSans } from '../../fonts';
 
 const PreloaderWrapper = styled.div<{ visible: boolean }>`
   width: 100%;
@@ -16,6 +17,8 @@ const PreloaderWrapper = styled.div<{ visible: boolean }>`
   overflow: hidden;
   background: url(${background}) no-repeat center;
   background-size: 100% 100%;
+  align-items: flex-end;
+  justify-content: center;
 `;
 
 const cloudMove = keyframes`
@@ -44,12 +47,56 @@ const Cloud = styled.div<ICloud>`
   z-index: ${props => props.zIndex || 1};
 `;
 
+const LoadingLine = styled.div<{ persentOfLoad?: number }>`
+  width: 659px;
+  height: 23px;
+  box-shadow: inset 0 1px 4px 0 #202d3d, inset -1px 0 4px 0 #202d3d;
+  background-color: #233742;
+  z-index: 3;
+  transform: skew(-30deg);
+  margin-bottom: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  span {
+    text-shadow: 1px 1px 0 #233742;
+    font-family: ${MTSSans.ULTRA_WIDE};
+    font-size: 20px;
+    line-height: 2;
+    letter-spacing: -0.31px;
+    color: #ffffff;
+    transform: skew(30deg);
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: ${props => props.persentOfLoad}%;
+    box-shadow: inset 0 1px 3px 0 rgba(255, 255, 255, 0.5);
+    background-image: linear-gradient(
+      to bottom,
+      #bfdcff 1%,
+      #8bd2ff 38%,
+      #7ec7ff 53%,
+      #8ad1ff 99%
+    );
+  }
+`;
+
 export const Preloader: React.FC<{ visible: boolean }> = ({ visible }) => {
   return (
     <PreloaderWrapper visible={visible}>
       {cloudsConfig.map(cloud => (
         <Cloud key={cloud.keyId} {...cloud} />
       ))}
+      <LoadingLine persentOfLoad={50}>
+        <span>50%</span>
+      </LoadingLine>
     </PreloaderWrapper>
   );
 };
