3344ec2afc28db2f26fa13ec2d6c9f27c16309be
DVasilkov <dvasilkov@stream.ru>
2020-05-30 20:10:25 +0300

    edit animation wave

--- src/components/waves/index.tsx
+++ src/components/waves/index.tsx
@@ -5,20 +5,22 @@ import wave3 from './wave3.png';
 import styled, { keyframes, Keyframes } from 'styled-components';
 
 const testAnim = keyframes`
+
 0% {
-    opacity: 1;
-    transform: translate3d(-15px, -27px, 0px) scale(0.85);
-    /* transform: translate3d(-25px, -62px, 0px) scale(0.75) ; */
-}
-50% {
-    opacity: 1;
+  background-position: left 0px top 0px ;
+  opacity: 0.2;
 }
 
+25% {
+  opacity: 1;
+}
+75% {
+  opacity: 1;
+}
 
 100% {
-    opacity : 0;
-    transform: translate3D(0px, 30px, 0) perspective(100px) rotate3d(1, 0, 1, 5deg) skew(-25.5deg);
-    
+  opacity: 0;
+  background-position: left 0px top 242px;
 }
 `;
 
@@ -30,7 +32,7 @@ to {
 
 `;
 
-const WaveImg = styled.img<IWaveImg>`
+const WaveImg = styled.div<IWaveImg>`
   width: ${props => props.width};
   height: ${props => props.height};
   position: absolute;
@@ -38,54 +40,51 @@ const WaveImg = styled.img<IWaveImg>`
   left: ${props => props.left};
   z-index: 500;
   transform: ${props => props.transform};
-  animation: ${props => props.animation} infinite;
+  animation: ${props => props.animation} infinite linear;
   animation-duration: ${props => props.animationDuration || '4s'};
   animation-delay: ${props => props.animationDelay};
-  animation-direction: ${props => props.amimationDirection};
+  animation-direction: ${props => props.animationDirection};
+  background: url(${props => props.background});
+  background-size: 100% 100%;
+  background-repeat: repeat-y;
+  border-radius: 0% 100% 70% 12% / 46% 43% 61% 14%;
 `;
 
 const mockConfig: IWaveImg[] = [
   {
-    width: '128px',
-    height: '174px',
-    top: '38.4%',
-    left: '56.5%',
+    width: '155px',
+    height: '242px',
+    top: '37.9%',
+    left: '56.4%',
     animation: testAnim,
+    animationDuration: '6s',
   },
   {
     width: '165px',
     height: '157px',
     top: '38.7%',
     left: '56.4%',
-    animation: testAnim2,
-    animationDuration: '2s',
-    amimationDirection: 'alternate-reverse',
+    // animation: testAnim2,
+    // animationDuration: '2s',
+    // animationDirection: 'alternate-reverse',
   },
   {
     width: '165px',
     height: '136px',
     top: '37.1%',
     left: '56.4%',
-    animation: testAnim2,
-    animationDuration: '4s',
-    amimationDirection: 'alternate-reverse',
-  },
-  {
-    width: '128px',
-    height: '174px',
-    top: '38.4%',
-    left: '56.5%',
-    animation: testAnim,
-    animationDelay: '1s',
+    // animation: testAnim2,
+    // animationDuration: '4s',
+    // animationDirection: 'alternate-reverse',
   },
 ];
 
 export const Waves: React.FC = () => {
   return (
     <>
-      <WaveImg {...mockConfig[0]} src={wave1} alt="wave"></WaveImg>
-      <WaveImg {...mockConfig[1]} src={wave2} alt="wave"></WaveImg>
-      <WaveImg {...mockConfig[2]} src={wave3} alt="wave"></WaveImg>
+      <WaveImg {...mockConfig[0]} background={wave1}></WaveImg>
+      <WaveImg {...mockConfig[1]} background={wave2}></WaveImg>
+      <WaveImg {...mockConfig[2]} background={wave3}></WaveImg>
     </>
   );
 };
@@ -99,5 +98,6 @@ interface IWaveImg {
   animation?: Keyframes;
   animationDelay?: string;
   animationDuration?: string;
-  amimationDirection?: string;
+  animationDirection?: string;
+  background?: string;
 }
