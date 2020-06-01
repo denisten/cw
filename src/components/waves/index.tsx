ea4af1bd008dcea509ff62a86bf533cf3d5a908a
DVasilkov <dvasilkov@stream.ru>
2020-06-01 15:42:14 +0300

    create wave animation logic

--- src/components/waves/index.tsx
+++ src/components/waves/index.tsx
@@ -11,25 +11,18 @@ const testAnim = keyframes`
   opacity: 0.2;
 }
 
-25% {
+15% {
   opacity: 1;
 }
-75% {
+
+85% {
   opacity: 1;
 }
 
 100% {
   opacity: 0;
-  background-position: left 0px top 242px;
+  background-position: left 0px top 160px;
 }
-`;
-
-const testAnim2 = keyframes`
-to {
-  transform: scale(0.9) skew(-15deg);
-  opacity: 0.8;
-}
-
 `;
 
 const WaveImg = styled.div<IWaveImg>`
@@ -45,49 +38,160 @@ const WaveImg = styled.div<IWaveImg>`
   animation-delay: ${props => props.animationDelay};
   animation-direction: ${props => props.animationDirection};
   background: url(${props => props.background});
-  background-size: 100% 100%;
-  background-repeat: repeat-y;
-  border-radius: 0% 100% 70% 12% / 46% 43% 61% 14%;
+  background-size: ${props => props.backgroundSize || '100% 100%'};
+  background-repeat: ${props => props.backgroundRepeat || 'no-repeat'};
+  border-radius: ${props => props.borderRadius || '0px'};
+  animation-fill-mode: both;
+`;
+
+const testAnim2 = keyframes`
+
+0% {
+  opacity: .6;
+}
+
+50% {
+  opacity: 1;
+  transform: translate3D(-15px, 15px, 0);
+  /* transform: translate3D(-40px, 10px, 0); */
+}
+70% {
+  opacity: 0;
+}
+
+100% {
+  opacity: 0;
+  transform: translate3D(-15px, 15px, 0);
+  /* transform: translate3D(-40px, 10px, 0); */
+}
+`;
+
+const testAnim3 = keyframes`
+
+0% {
+  opacity: .6;
+}
+
+50% {
+  opacity: 1;
+  transform: translate3D(5px, 10px, 0);
+  /* transform: translate3D(-40px, 10px, 0); */
+}
+70% {
+  opacity: 0;
+}
+
+100% {
+  opacity: 0;
+  transform: translate3D(5px, 10px, 0);
+  /* transform: translate3D(-40px, 10px, 0); */
+}
 `;
 
 const mockConfig: IWaveImg[] = [
   {
-    width: '155px',
-    height: '242px',
-    top: '37.9%',
+    width: '140px',
+    height: '218px',
+    top: '38.1%',
     left: '56.4%',
     animation: testAnim,
-    animationDuration: '6s',
+    animationDuration: '4s',
+    borderRadius: '0% 100% 70% 12% / 46% 43% 61% 14%',
+    backgroundRepeat: 'repeat',
+    background: wave1,
+    backgroundSize: 'contain',
   },
   {
-    width: '165px',
-    height: '157px',
-    top: '38.7%',
-    left: '56.4%',
-    // animation: testAnim2,
-    // animationDuration: '2s',
-    // animationDirection: 'alternate-reverse',
+    width: '143px',
+    height: '132px',
+    top: '39.6%',
+    left: '56.5%',
+    background: wave2,
   },
   {
-    width: '165px',
-    height: '136px',
-    top: '37.1%',
-    left: '56.4%',
-    // animation: testAnim2,
-    // animationDuration: '4s',
-    // animationDirection: 'alternate-reverse',
+    width: '246px',
+    height: '122px',
+    top: '37.3%',
+    left: '56.2%',
+    background: wave3,
+  },
+  {
+    width: '127px',
+    height: '112px',
+    top: '57.8%',
+    left: '30.4%',
+    animation: testAnim2,
+    animationDuration: '3s',
+    backgroundRepeat: 'no-repeat',
+    background: wave1,
+    backgroundSize: '100% 100%',
+  },
+  {
+    width: '110px',
+    height: '100px',
+    top: '59.1%',
+    left: '29%',
+    animation: testAnim2,
+    animationDuration: '3s',
+    animationDelay: '1.5s',
+    backgroundRepeat: 'no-repeat',
+    background: wave1,
+    backgroundSize: '100% 100%',
+  },
+  {
+    width: '177px',
+    height: '148px',
+    top: '57.8%',
+    left: '29.7%',
+    background: wave2,
+  },
+  {
+    width: '45px',
+    height: '40px',
+    top: '65%',
+    left: '22.45%',
+    backgroundRepeat: 'no-repeat',
+    background: wave1,
+    backgroundSize: '100% 100%',
+    animationDuration: '3s',
+    animation: testAnim3,
+  },
+  {
+    width: '45px',
+    height: '40px',
+    top: '65.7%',
+    left: '22.3%',
+    backgroundRepeat: 'no-repeat',
+    background: wave1,
+    backgroundSize: '100% 100%',
+    animationDuration: '3s',
+    animationDelay: '1.5s',
+    animation: testAnim3,
+  },
+
+  {
+    width: '140px',
+    height: '218px',
+    top: '65.7%',
+    left: '22.3%',
+    animation: testAnim,
+    animationDuration: '4s',
+    borderRadius: '0% 100% 70% 12% / 46% 43% 61% 14%',
+    backgroundRepeat: 'repeat',
+    background: wave1,
+    backgroundSize: 'contain',
   },
 ];
 
-export const Waves: React.FC = () => {
+export const Waves: React.FC = React.memo(() => {
   return (
     <>
-      <WaveImg {...mockConfig[0]} background={wave1}></WaveImg>
-      <WaveImg {...mockConfig[1]} background={wave2}></WaveImg>
-      <WaveImg {...mockConfig[2]} background={wave3}></WaveImg>
+      {mockConfig.map((waveParams, ind) => (
+        <WaveImg key={ind} {...waveParams} />
+      ))}
     </>
   );
-};
+});
 
 interface IWaveImg {
   width: string;
@@ -100,4 +204,7 @@ interface IWaveImg {
   animationDuration?: string;
   animationDirection?: string;
   background?: string;
+  backgroundRepeat?: string;
+  borderRadius?: string;
+  backgroundSize?: string;
 }
