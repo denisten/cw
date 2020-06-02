9bb51befd2840e1709fb310c734934edccac6941
DVasilkov <dvasilkov@stream.ru>
2020-06-02 18:46:48 +0300

    fix - working in firefox right now

--- src/components/waves/svg-filter.tsx
+++ src/components/waves/svg-filter.tsx
@@ -15,8 +15,7 @@ export const svgFilter = (
         attributeName="baseFrequency"
         dur="6s"
         keyTimes="0;0.5;1"
-        from="0.02"
-        by="0.05"
+        values="0.02 0.05; 0.04 0.07; 0.06 0.09"
         repeatCount="indefinite"
       />
     </filter>
