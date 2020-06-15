8885ead79fe3904b5d127c1373dbe79d8b73109a
DVasilkov <dvasilkov@stream.ru>
2020-06-15 10:34:27 +0300

    added main common interface for incomes and balance data

--- src/api/commit-income/index.ts
+++ src/api/commit-income/index.ts
@@ -3,13 +3,13 @@ import { apiRoutes } from '..';
 import { TowersTypes } from '../../effector/towers-progress/store';
 
 export const commitIncomes = async (towerTitle: TowersTypes) => {
-  const response = await post<ICommitIncomes>(
+  const response = await post<IIncomesData>(
     apiRoutes.PRODUCTS + towerTitle + apiRoutes.COMMIT_INCOMES
   );
   return response.data;
 };
 
-export interface ICommitIncomes {
+export interface IIncomesData {
   state: string;
   data: { id: number; userId: number; balance: number };
 }
