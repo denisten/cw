266cd1c062f39769e9d21674e0aa5c0e142f3d6f
DVasilkov <dvasilkov@stream.ru>
2020-06-22 11:19:06 +0300

    deleted tail in move-coin

--- src/components/move-coin-collection/move-coin-elem/index.tsx
+++ src/components/move-coin-collection/move-coin-elem/index.tsx
@@ -2,36 +2,13 @@ import React from 'react';
 import styled, { keyframes, Keyframes } from 'styled-components';
 import { ZIndexes } from '../../root-component/z-indexes-enum';
 import coinBg from '../../../UI/icons/coin.svg';
-import tailAnim from './anim_tail.png';
-import { Sprite } from '../../sprite';
+
 import {
   removeMoveElems,
   setMoveCoinFinished,
 } from '../../../effector/reward/events';
 import { IMoveCoinElement } from '../../../effector/reward/store';
 
-const scaleTile = keyframes`
-0% {
-    transform: scale(0.5);
-}
-
-50% {
-  opacity: 1;
-  transform: scale(1);
-}
-
-75% {
-  opacity: 0.8;
-  transform: scale(1.1);
-}
-
-100% {
-    transform: scale(1.15);
-    opacity: 0;
-}
-`;
-
-const delta = 200;
 const coinMoveAnimationDuration = 1000;
 
 const MoveCoin = styled.div`
@@ -55,26 +32,6 @@ const MoveWrapper = styled.div<ImoveCoin>`
   user-select: none;
 `;
 
-const SpriteWrapper = styled.div`
-  animation: ${scaleTile} ${coinMoveAnimationDuration - delta}ms linear forwards;
-`;
-
-const commonSpriteSetting = {
-  canvasWidth: 70,
-  canvasHeight: 223,
-  numberOfFramesX: 7,
-  numberOfFramesY: 2,
-  ticksPerFrame: 4,
-  infinity: true,
-
-  style: {
-    position: 'absolute',
-    top: '22px',
-    left: '10px',
-    width: '120px',
-    height: '80px',
-  } as React.CSSProperties,
-};
 const shiftDelta = { x: 130, y: 100 };
 const keyframeGenerator = (x: number, y: number) => {
   return keyframes`
@@ -111,9 +68,6 @@ export const MoveCoinElem: React.FC<IMoveCoinElement> = ({ x, y, id }) => {
       keyframe={keyframeGenerator(x, y)}
     >
       <MoveCoin />
-      <SpriteWrapper onAnimationEnd={e => e.stopPropagation()}>
-        <Sprite {...commonSpriteSetting} img={tailAnim} />
-      </SpriteWrapper>
     </MoveWrapper>
   );
 };
