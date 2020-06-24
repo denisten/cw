fc4a923d16757016ec602e6e5be8727480406570
DVasilkov <dvasilkov@stream.ru>
2020-06-24 15:07:02 +0300

    move setting items to other component

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -2,15 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useStore } from 'effector-react';
 import { SettingsStore, SettingsType } from '../../effector/settings/store';
-import notification from './notification.svg';
-import notificationActive from './notification-active.svg';
-import sound from './sound.svg';
-import soundActive from './sound-active.svg';
-import music from './music.svg';
-import musicActive from './music-active.svg';
-import language from './language.svg';
-import languageActive from './language-active.svg';
-import { musicAndSoundToggle } from '../../effector/settings/events';
+import { SettingItems } from './setting-items';
 
 const SettingWrapper = styled.div`
   width: 100%;
@@ -32,36 +24,9 @@ const SettingOptionsWrapper = styled.div`
   display: flex;
 `;
 
-const returnOptionBackground = (active: boolean, type: SettingsType) => {
-  switch (type) {
-    case SettingsType.NOTIFICATION:
-      return active ? notificationActive : notification;
-    case SettingsType.SOUND:
-      return active ? soundActive : sound;
-    case SettingsType.MUSIC:
-      return active ? musicActive : music;
-    case SettingsType.LANGUAGE:
-      return active ? languageActive : language;
-
-    default:
-      break;
-  }
-};
-
-const Option = styled.div<IOption>`
-  width: 60px;
-  height: 60px;
-  background-size: 100% 100%;
-  background: url(${props => returnOptionBackground(props.active, props.type)})
-    no-repeat center;
-  &:not(:last-child) {
-    margin-right: 20px;
-  }
-`;
-
-const SettingPopUp = styled.div`
-  width: 300px;
-`;
+// const SettingPopUp = styled.div`
+//   width: 300px;
+// `;
 
 export const Settings: React.FC = () => {
   const { sound, music } = useStore(SettingsStore);
@@ -79,60 +44,18 @@ export const Settings: React.FC = () => {
   };
 
   // console.log(notification, sound, music, language);
-  const settingsItemCreate = () => {
-    const mainSetting = Object.values(SettingsType).map((elem, ind) => {
-      switch (elem) {
-        case SettingsType.NOTIFICATION:
-          return (
-            <Option
-              key={ind}
-              active={selectOptionPopUpType === SettingsType.NOTIFICATION}
-              onClick={() => selectPopUpItem(SettingsType.NOTIFICATION)}
-              type={elem}
-            />
-          );
-        case SettingsType.SOUND:
-          return (
-            <Option
-              key={ind}
-              active={sound}
-              type={elem}
-              onClick={() =>
-                musicAndSoundToggle({ settingType: elem, flag: !sound })
-              }
-            />
-          );
-        case SettingsType.MUSIC:
-          return (
-            <Option
-              key={ind}
-              active={music}
-              type={elem}
-              onClick={() =>
-                musicAndSoundToggle({ settingType: elem, flag: !music })
-              }
-            />
-          );
-        case SettingsType.LANGUAGE:
-          return (
-            <Option
-              key={ind}
-              active={selectOptionPopUpType === SettingsType.LANGUAGE}
-              onClick={() => selectPopUpItem(SettingsType.LANGUAGE)}
-              type={elem}
-            />
-          );
-        default:
-          break;
-      }
-    });
-    return mainSetting;
-  };
 
   return (
     <SettingWrapper>
       <SettingPopUpWrapper></SettingPopUpWrapper>
-      <SettingOptionsWrapper>{settingsItemCreate()}</SettingOptionsWrapper>
+      <SettingOptionsWrapper>
+        <SettingItems
+          callback={selectPopUpItem}
+          selectOptionPopUpType={selectOptionPopUpType}
+          soundState={sound}
+          musicState={music}
+        />
+      </SettingOptionsWrapper>
     </SettingWrapper>
   );
 };
---
+++ src/components/settings/setting-items/index.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { SettingsType } from '../../../effector/settings/store';
+import styled from 'styled-components';
+import notification from './notification.svg';
+import notificationActive from './notification-active.svg';
+import sound from './sound.svg';
+import soundActive from './sound-active.svg';
+import music from './music.svg';
+import musicActive from './music-active.svg';
+import language from './language.svg';
+import languageActive from './language-active.svg';
+import { musicAndSoundToggle } from '../../../effector/settings/events';
+
+const returnOptionBackground = (active: boolean, type: SettingsType) => {
+  switch (type) {
+    case SettingsType.NOTIFICATION:
+      return active ? notificationActive : notification;
+    case SettingsType.SOUND:
+      return active ? soundActive : sound;
+    case SettingsType.MUSIC:
+      return active ? musicActive : music;
+    case SettingsType.LANGUAGE:
+      return active ? languageActive : language;
+
+    default:
+      break;
+  }
+};
+
+const Option = styled.div<IOption>`
+  width: 60px;
+  height: 60px;
+  background-size: 100% 100%;
+  background: url(${props => returnOptionBackground(props.active, props.type)})
+    no-repeat center;
+  &:not(:last-child) {
+    margin-right: 20px;
+  }
+`;
+
+export const SettingItems: React.FC<ISettingItems> = ({
+  callback,
+  selectOptionPopUpType,
+  soundState,
+  musicState,
+}) => {
+  const items = Object.values(SettingsType).map((elem, ind) => {
+    switch (elem) {
+      case SettingsType.NOTIFICATION:
+        return (
+          <Option
+            key={ind}
+            active={selectOptionPopUpType === SettingsType.NOTIFICATION}
+            onClick={() => callback(SettingsType.NOTIFICATION)}
+            type={elem}
+          />
+        );
+      case SettingsType.SOUND:
+        return (
+          <Option
+            key={ind}
+            active={soundState}
+            type={elem}
+            onClick={() =>
+              musicAndSoundToggle({ settingType: elem, flag: !soundState })
+            }
+          />
+        );
+      case SettingsType.MUSIC:
+        return (
+          <Option
+            key={ind}
+            active={musicState}
+            type={elem}
+            onClick={() =>
+              musicAndSoundToggle({ settingType: elem, flag: !musicState })
+            }
+          />
+        );
+      case SettingsType.LANGUAGE:
+        return (
+          <Option
+            key={ind}
+            active={selectOptionPopUpType === SettingsType.LANGUAGE}
+            onClick={() => callback(SettingsType.LANGUAGE)}
+            type={elem}
+          />
+        );
+      default:
+        break;
+    }
+  });
+
+  return <>{items}</>;
+};
+
+interface ISettingItems {
+  callback: (elem: SettingsType) => void;
+  selectOptionPopUpType: SettingsType | '';
+  soundState: boolean;
+  musicState: boolean;
+}
+
+interface IOption {
+  active: boolean;
+  type: SettingsType;
+}
