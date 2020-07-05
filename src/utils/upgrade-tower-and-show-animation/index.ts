fd23708ae8edd1b41844bb97e46efa8721cdc499
DVasilkov <dvasilkov@stream.ru>
2020-07-05 14:24:44 +0300

    fix towerUpgrade in tutorial mode

--- src/utils/upgrade-tower-and-show-animation/index.ts
+++ src/utils/upgrade-tower-and-show-animation/index.ts
@@ -1,12 +1,13 @@
 import { TowersTypes } from '../../effector/towers-progress/store';
 import { showUpgradeIcon } from '../../effector/app-condition/events';
-import { upgradeTower } from '../../effector/towers-progress/events';
+import { tutorialTowerUpgrade } from '../../effector/towers-progress/events';
 
 const delayBeforeAnimationEnd = 2500;
 
 export const upgradeTowerAndShowAnimation = (towerTitle: TowersTypes) => {
   showUpgradeIcon(towerTitle);
   setTimeout(() => {
-    upgradeTower(towerTitle);
+    tutorialTowerUpgrade(towerTitle);
+    showUpgradeIcon(null);
   }, delayBeforeAnimationEnd);
 };
