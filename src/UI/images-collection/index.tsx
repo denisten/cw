affc64248558ce4278e95841422dbbf229040fad
Тен Денис Сергеевич <dten@stream.ru>
2020-07-05 16:39:47 +0300

    fixed slider el size, overwrote inline img tag

--- src/UI/images-collection/index.tsx
+++ src/UI/images-collection/index.tsx
@@ -1,16 +1,20 @@
 import React, { memo } from 'react';
-import styled from 'styled-components';
 
-const ImageCollectionElement = styled.img`
-  width: 781px;
-  height: 301px;
-`;
+const defaultElementWidth = '781px',
+  defaultElementHeight = '301px';
+
 export const ImagesCollection: React.FC<IImagesCollection> = memo(
   ({ imgArray }) => {
     return (
       <>
         {imgArray.map(el => (
-          <ImageCollectionElement src={el} key={el} alt="imgContent" />
+          <img
+            src={el}
+            key={el}
+            alt="imgContent"
+            width={defaultElementWidth}
+            height={defaultElementHeight}
+          />
         ))}
       </>
     );
