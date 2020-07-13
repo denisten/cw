c80b0275ee103a10b50652a4cba00f96ea032ce7
Тен Денис Сергеевич <dten@stream.ru>
2020-07-13 20:00:58 +0300

    fixed rerenders. now only one

--- src/components/tutorial-slider/index.tsx
+++ src/components/tutorial-slider/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { Overlay } from '../../UI/overlay';
 import background from './background.svg';
@@ -118,62 +118,69 @@ const calculateCurrentStep = (step: number, arrLength: number) => {
   else return step > 0 ? Math.abs(currentStep) : arrLength - 1 + currentStep;
 };
 
-export const TutorialSlider: React.FC<ITutorialSlider> = memo(
-  ({ displayFlag, content, imgArray, callback }) => {
-    const imageWrapperRef = useRef<HTMLDivElement>(null);
-    const [step, setStep] = useState(0);
-    const currentStep = calculateCurrentStep(step, imgArray.length);
-    useEffect(() => {
-      const request = requestAnimationFrame(() => {
-        if (imageWrapperRef.current) {
-          imageWrapperRef.current.style.transform = `translateX(${-currentStep *
-            defaultImgWrapperWidth}px)`;
-        }
-      });
-      return () => cancelAnimationFrame(request);
-    }, [step]);
+export const TutorialSlider: React.FC<ITutorialSlider> = ({
+  displayFlag,
+  content,
+  imgArray,
+  callback,
+}) => {
+  const imageWrapperRef = useRef<HTMLDivElement>(null);
+  const stepViewCollectionRef = useRef<HTMLDivElement>(null);
+  const step = useRef(0);
+  let prevStep = calculateCurrentStep(step.current, imgArray.length);
+  let currentStep = prevStep;
 
-    return (
-      <Overlay displayFlag={displayFlag} zIndex={ZIndexes.TUTORIAL_SLIDER}>
-        <TutorialSliderWrapper>
-          <ExitButton callBack={callback} {...styledConfig.exitButton} />
-          <ImageWrapper>
-            <ImageCollectionWrapper ref={imageWrapperRef}>
-              <ImagesCollection imgArray={imgArray} />
-            </ImageCollectionWrapper>
-          </ImageWrapper>
-          <DescriptionWrapper>
-            <Title content={content[currentStep].title} />
-            <Description content={content[currentStep].description} />
-            <DecreaseWrapper
-              src={decreaseImg}
-              alt="decrease"
-              onClick={() => setStep(step - 1)}
-            />
-            <IncreaseWrapper
-              src={increaseImg}
-              alt="increase"
-              onClick={() => setStep(step + 1)}
-            />
-            <StepView>
-              {imgArray.map((el, id) => {
-                let className = '';
-                if (id === currentStep) className = 'selected';
-                return (
-                  <StepViewElement
-                    className={className}
-                    key={el}
-                    onClick={() => setStep(id)}
-                  />
-                );
-              })}
-            </StepView>
-          </DescriptionWrapper>
-        </TutorialSliderWrapper>
-      </Overlay>
-    );
-  }
-);
+  const handleClick = (value: number, equal: boolean) =>
+    requestAnimationFrame(() => {
+      equal ? (step.current = value) : (step.current += value);
+      prevStep = currentStep;
+      currentStep = calculateCurrentStep(step.current, imgArray.length);
+      if (imageWrapperRef.current)
+        imageWrapperRef.current.style.transform = `translateX(${-currentStep *
+          defaultImgWrapperWidth}px)`;
+      stepViewCollectionRef.current?.children[prevStep].classList.remove(
+        'selected'
+      );
+      stepViewCollectionRef.current?.children[currentStep].classList.add(
+        'selected'
+      );
+    });
+
+  useEffect(() => {
+    stepViewCollectionRef.current?.children[0].classList.add('selected');
+  }, []);
+  return (
+    <Overlay displayFlag={displayFlag} zIndex={ZIndexes.TUTORIAL_SLIDER}>
+      <TutorialSliderWrapper>
+        <ExitButton callBack={callback} {...styledConfig.exitButton} />
+        <ImageWrapper>
+          <ImageCollectionWrapper ref={imageWrapperRef}>
+            <ImagesCollection imgArray={imgArray} />
+          </ImageCollectionWrapper>
+        </ImageWrapper>
+        <DescriptionWrapper>
+          <Title content={content[currentStep].title} />
+          <Description content={content[currentStep].description} />
+          <DecreaseWrapper
+            src={decreaseImg}
+            alt="decrease"
+            onClick={() => handleClick(-1, false)}
+          />
+          <IncreaseWrapper
+            src={increaseImg}
+            alt="increase"
+            onClick={() => handleClick(1, false)}
+          />
+          <StepView ref={stepViewCollectionRef}>
+            {imgArray.map((el, id) => (
+              <StepViewElement key={el} onClick={() => handleClick(id, true)} />
+            ))}
+          </StepView>
+        </DescriptionWrapper>
+      </TutorialSliderWrapper>
+    </Overlay>
+  );
+};
 
 interface ITutorialSlider {
   displayFlag: boolean;
