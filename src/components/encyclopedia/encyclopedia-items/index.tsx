b479e865c01841ed96bd6f70885dda34919bdc65
DVasilkov <dvasilkov@stream.ru>
2020-07-23 13:23:31 +0300

    added unknown button

--- src/components/encyclopedia/encyclopedia-items/index.tsx
+++ src/components/encyclopedia/encyclopedia-items/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import { IEncyclopediaItemConfig, EncyclopediaItemIds } from '..';
+import { MTSSans } from '../../../fonts';
+import icon from './icon.svg';
 
 const ItemsWrapper = styled.div`
   height: 100%;
   width: 308px;
   overflow: auto;
   flex-shrink: 0;
+  position: relative;
+  box-sizing: border-box;
+  padding-bottom: 100px;
 `;
 
 const EncyclopediaItem = styled.div<{ activElem: boolean }>`
@@ -29,6 +34,31 @@ const EncyclopediaItem = styled.div<{ activElem: boolean }>`
     background-color: #f3f6f8;
   }
 `;
+
+const ItemsButton = styled.div`
+  width: 100%;
+  position: absolute;
+  background: #02adc9;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  height: 100px;
+  bottom: 0;
+  left: 0;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  line-height: 33px;
+  color: #ffffff;
+  font-family: ${MTSSans.BOLD};
+`;
+
+const Icon = styled.img`
+  width: 26px;
+  height: 16px;
+  margin-right: 15px;
+`;
+
 export const EncyclopediaItems: React.FC<IEncyclopediaItems> = ({
   encyclopediaItemsList,
   activeItem,
@@ -43,7 +73,15 @@ export const EncyclopediaItems: React.FC<IEncyclopediaItems> = ({
       {item.label}
     </EncyclopediaItem>
   ));
-  return <ItemsWrapper>{list}</ItemsWrapper>;
+  return (
+    <ItemsWrapper>
+      {list}
+      <ItemsButton>
+        <Icon src={icon} alt="icon" />
+        <span>Обзор</span>
+      </ItemsButton>
+    </ItemsWrapper>
+  );
 };
 
 interface IEncyclopediaItems {
