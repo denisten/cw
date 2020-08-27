0fa2262a77cf9d1ee276beb161b87576920942e6
DVasilkov <dvasilkov@stream.ru>
2020-08-27 16:15:41 +0300

    fix get reward logic

--- src/effector/tasks-store/store.ts
+++ src/effector/tasks-store/store.ts
@@ -5,10 +5,8 @@ import {
   resetMissionsStore,
   saveTask,
   setCurrentTaskStatus,
-  takeReward,
   verifyTask,
 } from './events';
-import { editUserProperty } from '../user-data/events';
 import { TowersTypes } from '../towers-progress/store';
 import { chatTaskSession } from '../chat/events';
 import { TasksType } from '../../components/menu/menu-tasks';
@@ -31,14 +29,6 @@ export const TasksStore = MissionsDomain.store(initStore)
   .on(saveTask, (_, payload) => payload)
   .on(activateTask.doneData, (state, payload) => payload)
   .on(verifyTask.doneData, (state, payload) => payload)
-  .on(takeReward.doneData, (state, { id }) => {
-    const currentEl = state.findIndex(el => el.id === id);
-    const { money, energy } = state[currentEl];
-    editUserProperty({
-      money,
-      energy,
-    });
-  })
 
   .on(setCurrentTaskStatus, (state, { taskId, status }) => {
     const currentTaskIndex = state.findIndex(el => el.id === taskId);
