e08d5d1697e84f3ade079711c70daa3499c75082
Тен Денис Сергеевич <dten@stream.ru>
2020-09-15 20:58:44 +0300

    sounds hotfix

--- src/hooks/use-sound/index.ts
+++ src/hooks/use-sound/index.ts
@@ -1,15 +1,16 @@
-import { useState } from 'react';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const useAudio = (url: string, repeat: boolean, volume: number) => {
-  const [audio] = useState(new Audio(url));
-  audio.volume = volume;
-  audio.loop = repeat;
-
+  // const [audio] = useState(new Audio(url));
+  // audio.volume = volume;
+  // audio.loop = repeat;
+  //
   const play = () => {
-    audio.play();
+    // audio.play();
+    // console.log('no play');
   };
   const pause = () => {
-    audio.pause();
+    // audio.pause();
+    // console.log('no pause');
   };
 
   return { play, pause };
