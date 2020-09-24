49c6e0373902fa84b0b7e5472625083b040e0516
DVasilkov <dvasilkov@stream.ru>
2020-09-24 12:30:48 +0300

    export

--- src/utils/chat-utils/index.ts
+++ src/utils/chat-utils/index.ts
@@ -6,26 +6,28 @@ import { MissionsStore } from '../../effector/missions-store/store';
 const lettersPerSecond = 70,
   ms = 1000;
 
-const checkLastFailedMessage = (messages: IMessage[]) =>
+export const checkLastFailedMessage = (messages: IMessage[]) =>
   messages && messages[messages.length - 1]?.failedTask;
 
-const checkLastSuccessMessage = (messages: IMessage[]) =>
+export const checkLastSuccessMessage = (messages: IMessage[]) =>
   messages && messages[messages.length - 1]?.successTask;
 
-const isChatEnded = (messages: IMessage[]) =>
+export const isChatEnded = (messages: IMessage[]) =>
   checkLastFailedMessage(messages) || checkLastSuccessMessage(messages);
 
-const calculateMessageDelay = (letters: number) =>
+export const calculateMessageDelay = (letters: number) =>
   (letters / lettersPerSecond) * ms;
 
-const isLastAnswerBelongToUser = (messages: IMessage[]) =>
+export const isLastAnswerBelongToUser = (messages: IMessage[]) =>
   messages[messages.length - 1]?.direction === Sender.FRONTEND;
 
-const scrollToBottom = (chatContainerRef: React.RefObject<HTMLDivElement>) =>
+export const scrollToBottom = (
+  chatContainerRef: React.RefObject<HTMLDivElement>
+) =>
   chatContainerRef.current &&
   chatContainerRef.current.scrollTo(0, chatContainerRef.current.scrollHeight);
 
-const checkLastMessage = (
+export const checkLastMessage = (
   messages: IMessage[],
   setResponseStatus: () => void,
   setSavedMessages: () => void
@@ -37,30 +39,17 @@ const checkLastMessage = (
   }
 };
 
-const isLast = (idx: number, botLastMessages: IMessage[]) =>
+export const isLast = (idx: number, botLastMessages: IMessage[]) =>
   botLastMessages.length - 1 === idx;
 
-const findLastUserMessageIndex = (messages: IMessage[]) =>
+export const findLastUserMessageIndex = (messages: IMessage[]) =>
   messages.reduce(
     (acc, message, index) =>
       message.direction === Sender.FRONTEND ? index : acc,
     0
   );
 
-const findSubtask = (taskId: number): ITask | undefined =>
+export const findSubtask = (taskId: number): ITask | undefined =>
   MissionsStore.getState().find(mission =>
     mission.userSubTasks.find(subTask => subTask.id === taskId)
   );
-
-export {
-  checkLastFailedMessage,
-  checkLastSuccessMessage,
-  isChatEnded,
-  calculateMessageDelay,
-  isLastAnswerBelongToUser,
-  scrollToBottom,
-  checkLastMessage,
-  isLast,
-  findLastUserMessageIndex,
-  findSubtask,
-};
